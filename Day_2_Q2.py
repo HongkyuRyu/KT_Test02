730772de5189fb2cd085911992a5aaa2c20c66b9
HongkyuRyu <minsubrother@naver.com>
2023-02-03 15:36:14 +0900

    Day_2_Q2.py

--- Day_2_Q2.py
+++ Day_2_Q2.py
@@ -1,5 +1,6 @@
 # Q2 Answer template
 
+# 방법1
 def solution(numbers):
     answer = 0
     
@@ -10,4 +11,17 @@ def solution(numbers):
 
 numbers = [5, 8, 4, 0, 6, 7, 9]
 answer = solution(numbers)
+print(answer)
+
+
+# 방법2
+def solution2(numbers):
+    answer = 45
+    
+    for i in range(0, 10):
+        if i in numbers:
+            answer -= i
+    return answer
+numbers = [5, 8, 4, 0, 6, 7, 9]
+answer = solution2(numbers)
 print(answer)
