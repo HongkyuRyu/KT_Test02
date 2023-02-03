bd415694ed87eba85641edab4e4b02032e88e4e8
HongkyuRyu <69923886+HongkyuRyu@users.noreply.github.com>
2023-02-03 14:48:38 +0900

    Update Day_2_Q1.py

--- Day_2_Q1.py
+++ Day_2_Q1.py
@@ -1,5 +1,6 @@
 # Q1 Answer template
 
+# version 1) 2021-09-17
 def solution(lottos, win_nums):
     answer = []
     rank = [(0,0),(1,6), (2,5), (3,4), (4,3), (5,2), (6,1), (6,0)]
@@ -22,6 +23,27 @@ def solution(lottos, win_nums):
         if rank[i][1] == same_number:
             answer.append(rank[i][0])
     return answer
+
+# version 2: dic을 이용한 풀이 2023-02-03
+def solution2(lottos, win_nums):
+    # 0의 갯수 세기
+    zeros = lottos.count(0)
+    # 맞춘 갯수
+    correct = 0
+    # score 딕셔너리
+    scores = {0: 6, 1:6, 2:5, 3:4, 4:3, 5:2, 6:1} # x개 맞춤: 순위
+    
+    for l in lottos:
+        if l in win_nums:
+            correct += 1
+    
+    best_rank = correct + zeros # 맞춘 갯수 + 0이 다 맞았다고 한다면 : 가장 높은 순위임.
+    small_rank = correct # 맞춘 갯수만, 0은 다 틀렸다고 가정
+    
+    return [scores[best_rank], scores[small_rank]]
+
+
+
 
 # test case 1
 lottos = [44, 1, 0, 0, 31, 25]
